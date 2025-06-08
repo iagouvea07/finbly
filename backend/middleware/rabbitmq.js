244843fec0e2ccfd783b5b21adb966f08816d4e9
iagouvea07 <iagouvea@outlook.com>
2025-06-08 11:12:10 -0300

    helm

--- backend/middleware/rabbitmq.js
+++ backend/middleware/rabbitmq.js
@@ -10,7 +10,7 @@ async function connect() {
   if (connection === null) {
     try {
 
-      const rabbitmqUrl = `amqp://${process.env._DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`
+      const rabbitmqUrl = `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`
       connection = await amqp.connect(rabbitmqUrl);
       channel = await connection.createChannel();
 
