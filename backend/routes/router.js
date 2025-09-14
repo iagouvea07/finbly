1f7d97cec2f1f575652817da81cefaf682c287dc
Igor <iagouvea@outlook.com>
2025-09-14 20:49:38 -0300

    reset project

--- backend/routes/router.js
+++
@@ -1,5 +0,0 @@
-const postRouter = require('./post.js')
-const getRouter = require('./get.js')
-const putRouter = require('./put.js')
-
-module.exports = {postRouter, getRouter, putRouter}
--- frontend/src/components/input/input.js
+++
@@ -1,8 +0,0 @@
-'use client'
-import './input.css';
-
-const Input = (props) => {
-    return <input type={props.type} placeholder={props.placeholder} value={props.value} className="input" onChange={props.onChange}></input>
-}
-
-export default Input
